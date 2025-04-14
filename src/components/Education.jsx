38609f6723e81350fb3a3f1cdb520acea5b66bc9
Devansh Gupta <devansh00gupta@gmail.com>
2025-04-15 01:27:36 +0530

    corretced education E

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,5 @@
 import Lottie from "lottie-react";
-import education from "../assets/lottie/education.json";
+import education from "../assets/lottie/Education.json";
 import IIIT_KOTA from "../assets/IIIT_Kota.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
